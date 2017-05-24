2a56e0082e1513382b363caa23ec6dc4c8a1dcf5
Patrick Rauls <patrickrauls@Patricks-MacBook-Air.local>
2017-05-24 08:45:21 -0600

    extended requirements

--- test/numberConverter-test.js
+++ test/numberConverter-test.js
@@ -4,7 +4,7 @@ const { string_this_float } = require('../write_checks.js');
 describe('string_this_float', () => {
 
   it('returns One hundred', () => {
-    assert.deepEqual(string_this_float(100), 'One hundred and 00/100 dollars')
+    assert.deepEqual(string_this_float(100), 'One hundred dollars')
   })
   it('returns Two thousand, five hundred twenty-three and 04/100', () => {
     assert.deepEqual(string_this_float(2523.04), 'Two thousand five hundred twenty-three and 04/100 dollars')
@@ -12,13 +12,22 @@ describe('string_this_float', () => {
   it('returns Three and 14/100', () => {
     assert.deepEqual(string_this_float(Math.PI), 'Three and 14/100 dollars')
   })
-  it('returns Zero', () => {
+  it('returns Zero dollars', () => {
     assert.deepEqual(string_this_float(0), 'Zero dollars')
+  })
+  it('returns Zero dollars when given 0.001', () => {
+    assert.deepEqual(string_this_float(0.001), 'Zero dollars')
   })
   it('returns Zero and 84/100', () => {
     assert.deepEqual(string_this_float(0.84), 'Zero and 84/100 dollars')
   })
+  it('returns Negative zero and 84/100', () => {
+    assert.deepEqual(string_this_float(-0.84), 'Negative zero and 84/100 dollars')
+  })
   it('returns Fifty two and 00/100 dollars', () => {
-    assert.deepEqual(string_this_float(51.999), 'Fifty-two and 00/100 dollars')
+    assert.deepEqual(string_this_float(51.999), 'Fifty-two dollars')
+  })
+  it('returns One million dollars', () => {
+    assert.deepEqual(string_this_float(1000000), 'One million dollars')
   })
 });
--- write_checks.js
+++ write_checks.js
@@ -1,10 +1,10 @@
 let string_this_float = (float) => {
     let number = Math.abs(float),
         pennies = (number % 1).toFixed(2).slice(-2),
-        dollars = (number % 1).toFixed(2) == 1 ? '' + (Math.floor(number) + 1) : '' + Math.floor(number), //read somewhere coercion was faster than toString()
+        dollars = (number % 1).toFixed(2) == 1 ? '' + (Math.floor(number) + 1) : '' + Math.floor(number),
         magnitude = dollars.length,
         periods = [],
-        powers = ['', ' thousand', ' million'], //this can be expanded for greater amounts
+        powers = ['', ' thousand', ' million'],
         tens = ['twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'],
         units = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'],
         teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
@@ -12,6 +12,9 @@ let string_this_float = (float) => {
     for (let i = magnitude; i > 0; i -= 3) {
         //chunking this into groups of three
         periods.unshift(dollars.substring(i, i - 3))
+    }
+    if (pennies == 0 && dollars == 0) {
+        return 'Zero dollars';
     }
     let string_array = periods.map((period, index, array) => {
         if (period == 0) {
@@ -21,9 +24,9 @@ let string_this_float = (float) => {
         } else if (period < 20) {
             return teens[period - 10] + powers[array.length - index - 1] + ' ';
         } else if (period < 100 && period % 10 !== 0) {
-            return tens[Math.floor(period/10) - 2] + '-' + units[period[period.length - 1] - 1] + powers[array.length - index - 1] + ' ';
+            return tens[Math.floor(period / 10) - 2] + '-' + units[period[period.length - 1] - 1] + powers[array.length - index - 1] + ' ';
         } else if (period < 100) {
-            return tens[Math.floor(period/10) - 2] + powers[array.length - index - 1] + ' ';
+            return tens[Math.floor(period / 10) - 2] + powers[array.length - index - 1] + ' ';
         } else if (period % 100 > 19 && period % 10 === 0) {
             return units[period[0] - 1] + ' hundred ' + tens[period[1] - 2] + powers[array.length - index - 1] + ' ';
         } else if (period % 100 > 19) {
@@ -38,14 +41,22 @@ let string_this_float = (float) => {
     })
     let string = string_array.join('');
     //handle that negative
+    if (dollars == 0) {
+        string = 'zero and ' + pennies + '/100 dollars'
+    } else if (pennies != 0) {
+        string += 'and ' + pennies + '/100 dollars';
+    } else {
+        string += 'dollars'
+    }
+
     if (float < 0) {
         string = 'negative ' + string;
     }
-    dollars == 0 ? string = pennies + '/100 dollars' : string += 'and ' + pennies + '/100 dollars';
+
     string = string[0].toUpperCase() + string.slice(1);
     return string;
 }
 
 module.exports = {
-  string_this_float: string_this_float
+    string_this_float: string_this_float
 }
